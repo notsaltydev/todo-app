39c8c5ef4154e12df9536688d02e1654ed1371f5
adamnieslony <notsaltydev@gmail.com>
2017-05-03 16:22:55 +0200

    improvment app component adding the logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TodoDataService} from "./todo-data.service";
+import {Todo} from "./todo";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [TodoDataService]
 })
 export class AppComponent {
-  title = 'app works!';
+
+  newTodo: Todo = new Todo();
+
+  // Ask Angular DI system to inject the dependency
+  // associated with the dependency injection token `TodoDataService`
+  // and assign it to a property called `todoDataService`
+  constructor(private todoDataService: TodoDataService) {
+  }
+
+  get todos() {
+    return this.todoDataService.getAllTodos();
+  }
+
+  addTodo() {
+    this.todoDataService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  /**
+   * @param todo
+   */
+  removeTodo(todo) {
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  // Service is now available as this.todoDataService
+  /**
+   * @param todo
+   */
+  toggleTodoComplete(todo) {
+    this.todoDataService.toggleTodoComplete(todo);
+  }
 }
